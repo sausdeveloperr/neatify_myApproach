414f59f7a525608e047309e95eb495b50cc0f14d
Lekan <salamiolamilekan8991@gmail.com>
2025-05-16 19:26:19 +0100

    Test out some code for fetching src/dest dir paths

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
 import fs from 'fs';
 // import path from 'path'
-import moveFile from 'move-file';
+import { moveFile } from 'move-file';
 import inquirer from 'inquirer';
 
 // const src = path.join(__dirname,'source');
 // const dest = path.join(__dirname,'destination');
+
+inquirer.prompt([
+    {
+        name: 'src',
+        type: 'input',
+        message: 'input the source folder path or address'
+    }, 
+    {
+        name: 'dest',
+        type: 'input',
+        message: 'input the destination folder path or address'
+    }
+]).then(
+    (answers) => {
+        console.log(answers.src)
+        console.log(answers.dest)
+    }
+).catch(
+    (err) => {
+        console.info(`Error ${err} occured while parsing answers`)
+    }
+)
