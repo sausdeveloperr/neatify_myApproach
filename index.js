fd3755e6778b087f3952d826263a373ab666f840
Lekan <salamiolamilekan8991@gmail.com>
2025-05-16 18:23:29 +0100

    Initial commit - Created all necessary starting files and added import codes

---
+++ index.js
@@ -0,0 +1,7 @@
+import fs from 'fs';
+// import path from 'path'
+import moveFile from 'move-file';
+import inquirer from 'inquirer';
+
+// const src = path.join(__dirname,'source');
+// const dest = path.join(__dirname,'destination');
