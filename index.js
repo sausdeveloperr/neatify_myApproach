8874dce9b76e46fd7eda7e88744045d03bc3b449
Lekan <salamiolamilekan8991@gmail.com>
2025-05-21 21:14:52 +0100

    Complete code for helpers and file movement. Pending error to be treated

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ inquirer.prompt([
             return console.error(`Error reading dir: ${err}`);  
         }  
 
-        // no err? proceed to sorting..
+        // no err? proceed to sorting...
         console.log(`${files.length} files were found in the ${src} folder`); 
         const images = ['jpg', 'png', 'gif', 'tiff', 'bmp', 'svg', 'heic'],  
             audio = ['mp3', 'wav', 'm4a', 'aac', 'flac', 'ogg'],  
@@ -36,18 +36,22 @@ inquirer.prompt([
             code = ['json', 'html', 'css', 'js', 'xml', 'py', 'java', 'c', 'cpp', 'rb', 'php', 'go', 'swift', 'rust'],  
             others = ['zip', 'rar', '7z', 'exe', 'bat', 'ps1'];
 
+        // Create folders based on 5 criteria: images, audio, video, document, others using fs.mkdir
+        console.log(`Created 5 folders in ${dest} folder`);  
         /* 
             TODO
-            - add helper function for fs.exists/fs.mkdir which returns a true value if dir exists/created
-            - move logic below into each if condition above
+            - add helper function for fs.exists/fs.mkdir which returns a true value if dir exists/created ✅
+            - move logic below into each if condition above ✅
         */
 
         function dirMaker(dirName) {
-            const dirPath = `'/destination/${dirName}'`;
+            const dirPath = `'/${dest}/${dirName}'`;
 
             try {
-                if (!fs.existsSync(dirPath)) {
-                    fs.mkdirSync(dirPath);
+                if (fs.existsSync(dirPath)) {
+                    return true
+                } else {
+                    fs.mkdirSync(dirPath)
                     return true
                 }
             } catch (err) {
@@ -55,31 +59,34 @@ inquirer.prompt([
             }
         }
 
+        function fileMover(dirName) {
+            const currentFile = files[i];
+            const srcPath = path.join(src, currentFile);
+            const sortDir = `'/${dest}/${dirName}'`;
+            const destPath = path.join(sortDir, currentFile) 
+
+            moveFile(srcPath, destPath)
+        }
+
         for (let i = 0; i < files.length; i++) {
             const currentFileFormat = files[i].split('.').pop().toLowerCase();
             
             if (images.includes(currentFileFormat) && dirMaker(images)) {
-                
+                fileMover(images);
             } else if (audio.includes(currentFileFormat) && dirMaker(audio)) {
-
+                fileMover(audio);
             } else if (video.includes(currentFileFormat) && dirMaker(video)) {
-
+                fileMover(video);
             } else if (documents.includes(currentFileFormat) && dirMaker(documents)) {
-
+                fileMover(documents);
             } else if (code.includes(currentFileFormat) && dirMaker(code)) {
-
-            } else (others.includes(currentFileFormat) && dirMaker(others)) {
-
+                fileMover(code);
+            } else if (others.includes(currentFileFormat) && dirMaker(others)) {
+                fileMover(others);
+            } else {
+                console.log('File cannot be sorted into predefined groups');
             }
-            
-            // move logic below into each if condition above
-            const srcPath = path.join(src, currentFile)
-            const destPath = path.join(dest, currentFile) // issue arising here - How to make destPath select the path in dirPath?
-            moveFile(srcPath, destPath)
         }
-        
-        // Create folders based on 5 criteria: images, audio, video, document, others using fs.mkdir
-        console.log(`Created 5 folders in ${dest} folder`);  
     });  
 }).catch(
     (err) => {
