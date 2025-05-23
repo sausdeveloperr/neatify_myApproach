30c9f572cfdf307882287b2faa6793c325e7e87e
Lekan <salamiolamilekan8991@gmail.com>
2025-05-23 19:57:27 +0100

    Upgrade code to give better summary of organized files - main branch

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ inquirer.prompt([
             audioExt = ['mp3', 'wav', 'm4a', 'aac', 'flac', 'ogg'],  
             videoExt = ['mp4', 'mkv', 'avi', 'mov', 'webm', 'flv', 'mpeg', 'wmv'],  
             documentsExt = ['pdf', 'docx', 'doc', 'pptx', 'xlsx', 'odt', 'ods', 'csv', 'rtf', 'txt'],
-            codeExt = ['json', 'html', 'css', 'js', 'xml', 'py', 'java', 'c', 'cpp', 'rb', 'php', 'go', 'swift', 'rust'],  
+            codeExt = ['json', 'html', 'css', 'js', 'xml', 'py', 'java', 'c', 'cpp', 'rb', 'php', 'go', 'swift', 'rust', 'ts', 'sass', 'less', 'env', 'gitignore', 'yml'],  
             othersExt = ['zip', 'rar', '7z', 'exe', 'bat', 'ps1'];
 
         // Create folders based on 5 criteria: images, audio, video, document, others using fs.mkdir
@@ -65,27 +65,45 @@ inquirer.prompt([
         }
 
         var undefined = [];
+        let imgCount = 0, audCount = 0, vidCount = 0, docCount = 0, codeCount = 0, othersCount = 0;
         for (let i = 0; i < files.length; i++) {
             const currentFileFormat = files[i].split('.').pop().toLowerCase();
             
             if (imagesExt.includes(currentFileFormat) && dirMaker('images')) { 
                 fileMover('images', files[i]);
+                imgCount++;
             } else if (audioExt.includes(currentFileFormat) && dirMaker('audio')) {
                 fileMover('audio', files[i]);
+                audCount++;
             } else if (videoExt.includes(currentFileFormat) && dirMaker('video')) {
                 fileMover('video', files[i]);
+                vidCount++;
             } else if (documentsExt.includes(currentFileFormat) && dirMaker('documents')) {
                 fileMover('documents', files[i]);
+                docCount++;
             } else if (codeExt.includes(currentFileFormat) && dirMaker('code')) {
                 fileMover('code', files[i]);
+                codeCount++;
             } else if (othersExt.includes(currentFileFormat) && dirMaker('others')) {
                 fileMover('others', files[i]);
+                othersCount++;
             } else {
                 undefined.push(files[i]);
             }
         } 
-        console.log(`${files.length - undefined.length} files were sorted into ${dest} folders`);  
-        console.log(`${undefined.length} files could not be sorted into predefined groups: ${undefined}`);
+        console.log(`Organized ${files.length - undefined.length} files into ${dest} folders:
+                - images: ${imgCount}
+                - audio: ${audCount}
+                - videos: ${vidCount}
+                - documents: ${docCount}
+                - code files: ${codeCount}
+                - other files: ${othersCount}
+            `);  
+        console.log(`${undefined.length} files could not be sorted into predefined groups: ${undefined}`); 
+        
+        // TODO 
+        // account for resets of the undefined array and the count vars. Is it needed since I'm doing node index.js each time
+        // do you want to move unsorted files into 'undefined' folder or delete them - y yes, n no, d delete - NOT DECIDED YET!
     });  
 }).catch(
     (err) => {
